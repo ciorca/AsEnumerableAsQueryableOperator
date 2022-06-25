37830aca59c70ea412490272b7b332ee6af139ae
ciorca <ciorca.gaby@gmail.com>
2022-06-25 20:32:15 +0300

    Add project files.

---
+++ AsEnumerableAsQueryableOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsEnumerableAsQueryableOperator
+{
+    class Program
+    {
+        static void Main()
+        {
+            EmployeeDBDataContext dbContext = new EmployeeDBDataContext();
+            // TOP 5 Male Employees By Salary
+            var result = dbContext.Employees                         
+                           .Where(x => x.Gender == "Male")
+                           .AsEnumerable()
+                           .OrderByDescending(x => x.Salary).Take(5);
+            foreach (Employee e in result)
+            {
+                Console.WriteLine(e.Name + "\t" + e.Gender + "\t" + e.Salary);
+            }
+        }
+    }
+}
